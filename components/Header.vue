b270e859ef77dd1e1f70bddd8f2528dcd6d3f875
James C. Tucker <jamesctucker@protonmail.com>
2020-06-01 13:54:29 -0500

    added header

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="flex flex-row justify-between items-center p-4">
+    <div>
+      <nuxt-link to="/" class="nav-link m-2 text-xl font-bold">WCID</nuxt-link>
+    </div>
+    <div><Nav /></div>
+  </header>
+</template>
+
+<script>
+import Nav from "./Nav";
+export default {
+  name: "Header",
+  components: {
+    Nav
+  }
+};
+</script>
+
+<style scoped>
+.nav-link {
+  color: #202040;
+}
+</style>
