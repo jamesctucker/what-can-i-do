8b61c33bdd45b81b208231c6947b9f633a9e38a2
James C. Tucker <jamesctucker@protonmail.com>
2020-06-01 13:54:37 -0500

    added navbar and header

---
+++ components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer class="flex flex-row justify-center items-center p-4 mt-8">
+    <p>
+      Made with a 💔 -
+      <a
+        href="https://github.com/jamesctucker/what-can-i-do.git"
+        target="_bank"
+        rel="noopener noreferrer"
+        >Contribute Here</a
+      >
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style scoped>
+footer {
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+}
+a {
+  color: #202040;
+  text-decoration: underline;
+}
+</style>
---
+++ components/Nav.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <nuxt-link to="/give" class="nav-link m-2">Give</nuxt-link>
+    <nuxt-link to="/read" class="nav-link m-2">Read</nuxt-link>
+    <nuxt-link to="/showup" class="nav-link m-2">Show Up</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Nav"
+};
+</script>
+
+<style scoped>
+.nav-link {
+  color: #202040;
+}
+
+.nav-link:hover {
+  color: rgb(255, 20, 147);
+}
+</style>
