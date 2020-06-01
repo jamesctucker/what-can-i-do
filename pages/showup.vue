f2b771b75d45706305cea0733343121d6270d711
James C. Tucker <jamesctucker@protonmail.com>
2020-06-01 15:12:16 -0500

    updated showup with recommended reading

--- pages/showup.vue
+++ pages/showup.vue
@@ -2,10 +2,31 @@
   <div class="container p-4">
     <section>
       <h1 class="p-2 mt-8 mb-2">✊ Show Up</h1>
-      <p class="p-2 mb-8">
+      <p class="p-2 mb-2">
         Make your voice heard by attending a protest. You can search for
-        protests near you on Twitter and Instagram.
+        protests near you on Twitter and Facebook.
       </p>
+    </section>
+    <section>
+      <h2 class="p-2 mt-4">Recommended Reading</h2>
+      <ul class="p-2 mb-4">
+        <li>
+          <a
+            href="https://www.aclu.org/know-your-rights/protesters-rights/"
+            target="_blank"
+            rel="noopener noreferrer"
+            >Know Your Rights As A Protestor</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://www.wired.com/story/how-to-protest-safely-surveillance-digital-privacy/"
+            target="_blank"
+            rel="noopener noreferrer"
+            >How To Protest Safely In An Age Of Surveillance</a
+          >
+        </li>
+      </ul>
     </section>
     <section>
       <img
