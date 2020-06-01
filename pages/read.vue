c5ad888bfdadbde238d852621a4ca62e546b3cf1
James C. Tucker <jamesctucker@protonmail.com>
2020-06-01 13:55:19 -0500

    setup reading view

---
+++ pages/read.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="container p-4">
+    <section>
+      <h1 class="p-2 mt-8 mb-2">📚 Read</h1>
+      <p class="p-2">
+        Take some time to dive deeper into the issue of racism. Broaden your
+        perspective. Reflect on the state of your own heart. Buy these books and
+        share them with your loved ones.
+      </p>
+    </section>
+    <section>
+      <h2 class="p-2">Books</h2>
+      <p class="p-2">
+        (These books were first suggested
+        <a
+          href="https://tatianamac.com/posts/white-guyde/"
+          target="_blank"
+          rel="noopener noreferrer"
+          >here</a
+        >
+        by the awesome
+        <a
+          href="https://twitter.com/TatianaTMac"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Tatiana Mac</a
+        >.)
+      </p>
+      <ul class="p-2 mt-4">
+        <li>
+          An Indigenous People's History of the United States (Roxanne
+          Dunbar-Ortiz)
+        </li>
+        <li>
+          How To Be Antiracist (Ibram X. Kendi)
+        </li>
+        <li>
+          How To Be Less Stupid About Race (Crystal M. Fleming)
+        </li>
+        <li>
+          So You Want To Talk About Race (Ijeoma Oluo)
+        </li>
+        <li>
+          The New Jim Crow (Michelle Alexander)
+        </li>
+        <li>
+          They Were Her Property (Stephanie E. Jones-Rogers)
+        </li>
+        <li>
+          White Fragility (Robin Diangelo)
+        </li>
+        <li>
+          White Tears, Brown Scars (Ruby Hamad)
+        </li>
+        <li>
+          Why I'm No Longer Talking About Race (Reni Eddo-Lodge)
+        </li>
+        <li>
+          Algorithms of Oppression (Safiya Noble)
+        </li>
+        <li>
+          Rage Inside the Machine (Rob Smith)
+        </li>
+        <li>
+          Technically Wrong (Sarah Watchter-Boettcher)
+        </li>
+      </ul>
+    </section>
+    <section>
+      <h2 class="p-2 mt-4">Articles</h2>
+      <ul class="p-2 mt-4">
+        <li>
+          <a
+            href="https://www.latimes.com/opinion/op-ed/la-xpm-2013-apr-07-la-oe-0407-silk-ring-theory-20130407-story.html"
+            >How Not To Say The Wrong Thing</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://www.theatlantic.com/politics/archive/2015/07/tanehisi-coates-between-the-world-and-me/397619/"
+            >Letter To My Son</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://theconversation.com/the-racist-roots-of-american-policing-from-slave-patrols-to-traffic-stops-112816"
+            >The Racist Roots of American Policing</a
+          >
+        </li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+a {
+  text-decoration: underline;
+}
+</style>
