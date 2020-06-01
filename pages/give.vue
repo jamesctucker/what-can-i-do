f7df465c6302cbb2cb6477d063efa43d0fded259
James C. Tucker <jamesctucker@protonmail.com>
2020-06-01 13:55:12 -0500

    setup giving view

---
+++ pages/give.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="container flex flex-col p-4">
+    <section>
+      <h1 class="p-2 mt-8 mb-2">❤️ Give</h1>
+      <p class="p-2">
+        One of the most powerful ways you can make a difference is through
+        donating. Here is a list of respected organizations that are currently
+        fighting for racial equality.
+      </p>
+    </section>
+    <section>
+      <ul class="p-2 mt-4">
+        <li>
+          <a
+            href="https://minnesotafreedomfund.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            >Minnesota Freedom Fund</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://unicornriot.ninja/donate/"
+            target="_blank"
+            rel="noopener noreferrer"
+            >Unicorn Riot</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://actionnetwork.org/fundraising/louisville-community-bail-fund/"
+            target="_blank"
+            rel="noopener noreferrer"
+            >Louisville Bail Fund</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://www.reclaimtheblock.org/home"
+            target="_blank"
+            rel="noopener noreferrer"
+            >Reclaim the Block</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://support.eji.org/give/153413/#!/donation/checkout"
+            target="_blank"
+            rel="noopener noreferrer"
+            >Equal Justice Initiative</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://colorofchange.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            >Color of Change</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://antiracismcenter.com/make-a-donation/"
+            target="_blank"
+            rel="noopener noreferrer"
+            >Anti-Racism Center</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://www.blackvisionsmn.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            >Black Visions Collective</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://www.gofundme.com/f/georgefloyd?fbclid=IwAR342hEyNUlpvaJbtbm0qgLiXUmURvaq845KXTlvIjQtgm5207WIjwimsf0"
+            target="_blank"
+            rel="noopener noreferrer"
+            >George Floyd Memorial Fund</a
+          >
+        </li>
+        <li>
+          <a
+            href="http://www.blacktablearts.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            >Black Table Arts</a
+          >
+        </li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Give"
+};
+</script>
+
+<style scoped></style>
