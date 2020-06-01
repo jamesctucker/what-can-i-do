93a836a54341e8c0a473de4d128672ee08caa55f
James C. Tucker <jamesctucker@protonmail.com>
2020-06-01 13:55:26 -0500

    setup showup view

---
+++ pages/showup.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container p-4">
+    <section>
+      <h1 class="p-2 mt-8 mb-2">✊ Show Up</h1>
+      <p class="p-2 mb-8">
+        Make your voice heard by attending a protest. You can search for
+        protests near you on Twitter and Instagram.
+      </p>
+    </section>
+    <section>
+      <img
+        src="/images/protestsafely.jpeg"
+        alt="tips for how to prepare for a protest"
+        class="p-2"
+      />
+      <a
+        href="https://twitter.com/todorokitaes/status/1266903510563651584/photo/1"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="p-2"
+        >Source</a
+      >
+    </section>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
